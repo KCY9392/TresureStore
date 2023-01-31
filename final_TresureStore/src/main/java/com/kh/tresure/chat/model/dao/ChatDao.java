a836574609b23291a404727af376c60d8e3c614e
Aox <92138800+Ssspil@users.noreply.github.com>
2023-01-31 16:25:07 +0900

    승필 - 안넣어진거 다시 넣기 채팅방
    유지보수

--- final_TresureStore/src/main/java/com/kh/tresure/chat/model/dao/ChatDao.java
+++ final_TresureStore/src/main/java/com/kh/tresure/chat/model/dao/ChatDao.java
@@ -15,106 +15,111 @@ import com.kh.tresure.chat.model.vo.ChatRoomJoin;
 
 @Repository
 public class ChatDao {
-	private Logger logger = LoggerFactory.getLogger(ChatDao.class);
-	
-	//채팅방 리스트 가져오기
-	public List<ChatRoom> selectChatRoomList(SqlSession sqlSession, int userNo) {
+   private Logger logger = LoggerFactory.getLogger(ChatDao.class);
+   
+   //채팅방 리스트 가져오기
+   public List<ChatRoom> selectChatRoomList(SqlSession sqlSession, int userNo) {
 
-		return sqlSession.selectList("chattingMapper.selectChatRoomList", userNo);
-	
-	}
+      return sqlSession.selectList("chattingMapper.selectChatRoomList", userNo);
+   
+   }
 
-	
-	//채팅방 나가기
-	public int exitChatRoom(SqlSession sqlSession, ChatRoomJoin join) {
+   
+   //채팅방 나가기
+   public int exitChatRoom(SqlSession sqlSession, ChatRoomJoin join) {
 
-		return sqlSession.delete("chattingMapper.exitChatRoom", join);
-	}
+      return sqlSession.delete("chattingMapper.exitChatRoom", join);
+   }
 
-	//차단 리스트 가져오기
-	public List<Block> selectBlockList(SqlSession sqlSession, int userNo) {
-		
-		return sqlSession.selectList("chattingMapper.selectBlockList", userNo);
-	}
-	
-	//차단 리스트에 추가
-	public int addBlock(SqlSession sqlSession, Block block) {
-	
-		return sqlSession.insert("chattingMapper.addBlock", block);
-	}
+   //차단 리스트 가져오기
+   public List<Block> selectBlockList(SqlSession sqlSession, int userNo) {
+      
+      return sqlSession.selectList("chattingMapper.selectBlockList", userNo);
+   }
+   
+   //차단 리스트에 추가
+   public int addBlock(SqlSession sqlSession, Block block) {
+   
+      return sqlSession.insert("chattingMapper.addBlock", block);
+   }
 
-	//차단 목록에서 삭제
-	public int deleteBlock(SqlSession sqlSession, Block block) {
+   //차단 목록에서 삭제
+   public int deleteBlock(SqlSession sqlSession, Block block) {
 
-		return sqlSession.delete("chattingMapper.deleteBlock", block);
-	}
-	
-	
-	// 메세지 입력하기
-	public int insertMessage(SqlSession sqlSession, ChatMessage message) {
-		
-		return sqlSession.insert("chattingMapper.insertMessage", message);
-	}
+      return sqlSession.delete("chattingMapper.deleteBlock", block);
+   }
+   
+   
+   // 메세지 입력하기
+   public int insertMessage(SqlSession sqlSession, ChatMessage message) {
+      
+      return sqlSession.insert("chattingMapper.insertMessage", message);
+   }
 
-	// 채팅방 참여확인
-	public static int joinCheck(SqlSession sqlSession, ChatRoomJoin join) {
-		
-		return sqlSession.selectOne("chattingMapper.joinCheck", join);
-	}
+   // 채팅방 참여확인
+   public static int joinCheck(SqlSession sqlSession, ChatRoomJoin join) {
+      
+      return sqlSession.selectOne("chattingMapper.joinCheck", join);
+   }
 
-	// 채팅방 참여
-	public static void inChatRoomJoin(SqlSession sqlSession, ChatRoomJoin join) {
-		
-		sqlSession.insert("chattingMapper.inChatRoomJoin", join);
-	}
+   // 채팅방 참여
+   public static void inChatRoomJoin(SqlSession sqlSession, ChatRoomJoin join) {
+      
+      sqlSession.insert("chattingMapper.inChatRoomJoin", join);
+   }
 
-	// 채팅방 메세지
-	public List<ChatMessage> selectChatMessageList(SqlSession sqlSession, ChatRoomJoin join) {
-		
-		return sqlSession.selectList("chattingMapper.selectChatMessageList", join);
-	}
+   // 채팅방 메세지
+   public List<ChatMessage> selectChatMessageList(SqlSession sqlSession, ChatRoomJoin join) {
+      
+      return sqlSession.selectList("chattingMapper.selectChatMessageList", join);
+   }
 
-	// 채팅방 생성하기
-	public int createChatRoom(SqlSession sqlSession, ChatRoom room) {
-		
-		int result =  sqlSession.insert("chattingMapper.createChatRoom", room);
-		
-		if(result > 0) {
+   // 채팅방 생성하기
+   public int createChatRoom(SqlSession sqlSession, ChatRoom room) {
+      
+      int result =  sqlSession.insert("chattingMapper.createChatRoom", room);
+      
+      if(result > 0) {
 
-			return room.getChatRoomNo();
-		} else {
-			return 0;
-		}
-	}
+         return room.getChatRoomNo();
+      } else {
+         return 0;
+      }
+   }
+
+   // 채팅방이 존재하는지 확인 (객체에 판매글 번호와 채팅 신청한 유저번호)
+   public int selectChatRoomByObject(SqlSession sqlSession, ChatRoom room) {
+      
+      return sqlSession.selectOne("chattingMapper.selectChatRoomByObject", room);
+   }
+
+   // 채팅방 번호가져오기
+   public int selectChatRoomNo(SqlSession sqlSession, ChatRoom room) {
+
+      return sqlSession.selectOne("chattingMapper.selectChatRoomNo", room);
+   }
+
+   // 채팅방번호로 인해 채팅방 정보가져오기
+   public ChatRoom selectChatRoomByNo(SqlSession sqlSession, int chatRoomNo) {
+
+      return sqlSession.selectOne("chattingMapper.selectChatRoomByNo", chatRoomNo);
+   }
+
+   // 채팅방 안에 구매자유저번호( 채팅방 만든 유저번호) 가져오기
+   public ChatRoom selectUserNoByChatRoomNo(SqlSession sqlSession, int chatRoomNo) {
+      
+      return sqlSession.selectOne("chattingMapper.selectUserNoByChatRoomNo", chatRoomNo);
+   }
 
 
-	// 채팅방이 존재하는지 확인 (객체에 판매글 번호와 채팅 신청한 유저번호)
-	public int selectChatRoomByObject(SqlSession sqlSession, ChatRoom room) {
-		
-		return sqlSession.selectOne("chattingMapper.selectChatRoomByObject", room);
-	}
-
-	// 채팅방 번호가져오기
-	public int selectChatRoomNo(SqlSession sqlSession, ChatRoom room) {
-
-		return sqlSession.selectOne("chattingMapper.selectChatRoomNo", room);
-	}
-
-	// 채팅방번호로 인해 채팅방 정보가져오기
-	public ChatRoom selectChatRoomByNo(SqlSession sqlSession, int chatRoomNo) {
-
-		return sqlSession.selectOne("chattingMapper.selectChatRoomByNo", chatRoomNo);
-	}
 
 
 
 
 
 
-
-
-	
-	
-	
-	
+   
+   
+   
+   
 }
--- final_TresureStore/src/main/java/com/kh/tresure/chat/model/vo/ChatRoom.java
+++ final_TresureStore/src/main/java/com/kh/tresure/chat/model/vo/ChatRoom.java
@@ -16,15 +16,16 @@ import lombok.NoArgsConstructor;
 @Data
 public class ChatRoom {
 
-	@DateTimeFormat(pattern = "yyyy/MM/dd HH:mm")
-	private int chatRoomNo; //채팅방 번호
-	private int userNo; // 채팅신청한 유저번호(구매자 유저번호)
-	private int sellNo; // 판매상품 번호
-	private String status; //채팅방 상태유무
-	private Date createDate; //채팅방 만든 시간
-	
-	private int sellUserNo; // 판매한 유저번호
-	private Timestamp chatDate; //대화한 시간
-	private int avg; //전체 거래 평점의 평균값
-	private String sellTitle; //상품제목
+   @DateTimeFormat(pattern = "yyyy/MM/dd HH:mm")
+   private int chatRoomNo; //채팅방 번호
+   private int userNo; // 채팅신청한 유저번호(구매자 유저번호)
+   private int sellNo; // 판매상품 번호
+   private String status; //채팅방 상태유무
+   private Date createDate; //채팅방 만든 시간
+   
+   private int sellUserNo; // 판매한 유저번호
+   private Timestamp chatDate; //대화한 시간
+   private double avg; // 판매자의평점
+   private String sellTitle; //상품제목
+   private double purchaseUserAvg;   // 구매자의 평점
 }
